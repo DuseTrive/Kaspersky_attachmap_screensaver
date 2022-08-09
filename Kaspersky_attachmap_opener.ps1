409065477dedec60262c654eac4d27a1c6ab7a2a
DuseTrive <78523790+DuseTrive@users.noreply.github.com>
2022-08-09 16:29:43 +0800

    Use Task scheduler and mouse movements.
    This is also another shorter variant of the previous PowerShell code which uses the task scheduler and schedules time to open and monitor mouse movement to stop.

---
+++ Kaspersky_attachmap_opener.ps1
@@ -0,0 +1,25 @@
+﻿$ie = New-Object -ComObject "InternetExplorer.Application"
+$ie.Navigate("https://cybermap.kaspersky.com/")
+$ie.Visible = $true
+$ie.FullScreen = $true
+$screensaver = 1
+Add-Type -AssemblyName System.Windows.Forms
+
+do
+{
+"IEHAS started: "
+"Getting First Messurements"
+$p1 = [System.Windows.Forms.Cursor]::Position
+Write-Output $p1.X, $p1.Y
+"Getting second Messurements"
+Start-Sleep -Seconds 1    # 15 minutses
+$p2 = [System.Windows.Forms.Cursor]::Position
+Write-Output $p2
+if ($p1.X -ne $p2.X -or $p1.Y -ne $p2.Y)
+    {
+    "iexplore shutting Down"
+    Start-Sleep -Seconds 1
+    Stop-Process -Name "iexplore"
+    $screensaver = 0
+    }
+    } while($screensaver -eq 1)
